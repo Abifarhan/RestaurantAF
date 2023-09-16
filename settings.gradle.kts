7d3a30e80929b1f44c20a4874af74451e17cd4ca
abi-aleph <abi.farhan@aleph-labs.com>
2023-09-16 13:20:07 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "RestaurantAF"
+include (":app")
+include(":login_feature")
+include(":component")
